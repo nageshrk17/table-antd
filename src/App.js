a6a5db719be974347161406bba7e1eae49e8fddf
nageshkulkarni17 <nkulkarni@qwinix.io>
2019-03-11 11:34:45 +0530

    Popup Columns

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Table } from 'antd';
 import 'antd/dist/antd.css';
-const { Column } = Table;
+import { Table, Checkbox, Button } from 'antd';
 
 
 const dataSource = [{
@@ -17,28 +16,99 @@ const dataSource = [{
   address: '10 Downing Street'
 }];
 
+
+let columns; 
+let arr = [];
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
       selected: null,
+      filteredColumns: [],
     };
+    this.onCheckBoxChange = this.onCheckBoxChange.bind(this);
+    this.onClickSubmit = this.onClickSubmit.bind(this);
+
+    columns = [
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        className: 'show',
+      }, {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+        className: 'show',
+      }, {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address',
+        className: 'show',
+        render: (text, record, index) => (
+          <span>
+          { this.state.selected === index ? <span>Button</span> : '' }
+          </span>
+        ),
+        filterDropdown: (
+          <div>
+            <div className="custom-filter-dropdown">
+              <Checkbox onChange={this.onCheckBoxChange} name='name'>Name</Checkbox>
+              <Checkbox onChange={this.onCheckBoxChange} name='age'>Age</Checkbox>
+            </div>
+            <div>
+              <Button type="primary" onClick={this.onClickSubmit}>Done</Button>
+            </div>
+          </div>
+        ),
+        onFilterDropdownVisibleChange: (visible) => {
+          console.log('wefewf');
+          
+          console.log('wefewf', this.state);
+        },
+      }
+     ];
   }
 
   showButton(rowIndex) {
     this.setState({ selected: rowIndex });
   }
 
-  hideButton(rowIndex) {
+  hideButton() {
     this.setState({ selected: null });
   }
 
+   onCheckBoxChange(e) {
+    if(e.target.checked) {
+      arr.push(e.target.name);
+      this.setState({ filteredColumns: arr });
+    } else {
+      arr.pop(e.target.name);
+      this.setState({ filteredColumns: arr });
+    }
+  }
+
+  onClickSubmit() {
+    const { filteredColumns } = this.state;
+    const col = columns && columns.map((item) => {
+      item.className = 'show';
+      filteredColumns.map((item1) => {
+        if (item.dataIndex === item1) {
+          item.className = 'hide';
+        }
+      });
+    });
+    this.setState(this.state)
+    return columns;
+   }
+
   render() {
-    const { selected } = this.state;
     return (
       <div className="App">
         <Table 
           dataSource={dataSource}
+          columns={columns}
           pagination={{ pageSize: 5 }} 
           onRow={(record, rowIndex) => {
             return {
@@ -46,33 +116,7 @@ class App extends Component {
               onMouseLeave: (event) => {this.hideButton()},
             };
           }}
-        > 
-          <Column
-            title="First Name"
-            dataIndex="firstName"
-            key="firstName"
-          />
-          <Column
-            title="Age"
-            dataIndex="age"
-            key="age"
-          />
-          <Column
-            title="Address"
-            dataIndex="address"
-            key="address"
-           />
-          <Column
-            title="Action"
-            dataIndex="Action"
-            key="action"
-            render={(text, record, index) => (
-              <span>
-              { selected === index ? <span>Button</span> : '' }
-              </span>
-            )}
-           />
-       </Table>
+        /> 
       </div>
     );
   }
