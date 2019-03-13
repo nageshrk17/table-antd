3164d32700bec1620cae7cca8dd6b7825bb7b9ed
nageshkulkarni17 <nkulkarni@qwinix.io>
2019-03-13 14:08:34 +0530

    Drag Drop

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import { Table, Checkbox, Button } from 'antd';
+import {sortableContainer, sortableElement} from 'react-sortable-hoc';
 
+const SortableItem = sortableElement(({value}) => <li>{value}</li>);
+
+const SortableContainer = sortableContainer(({children}) => {
+  return <ul>{children}</ul>;
+});
 
 const dataSource = [{
   key: 0,
@@ -17,6 +23,31 @@ const dataSource = [{
 }];
 
 
+const filterByColumn = [{
+  id: 1,
+  label: 'Name',
+  name: 'name',
+  checked: true,
+ },
+ {
+  id: 2,
+  label: 'Age',
+  name: 'age',
+  checked: true,
+ },{
+  id: 3,
+  label: 'Address',
+  name: 'address',
+  checked: true,
+ }];
+
+
+function arrayMove(array, from, to) {
+  array = array.slice();
+  array.splice(to < 0 ? array.length + to : to, 0, array.splice(from, 1)[0]);
+  return array;
+}
+
 let columns; 
 let arr = [];
 
@@ -26,9 +57,57 @@ class App extends Component {
     this.state = { 
       selected: null,
       filteredColumns: [],
+      items: [{
+        id: 1,
+        label: 'Name',
+        name: 'name',
+        checked: true,
+       },
+       {
+        id: 2,
+        label: 'Age',
+        name: 'age',
+        checked: true,
+       },
+       {
+        id: 3,
+        label: 'Address',
+        name: 'address',
+        checked: true,
+       }],
+       columns: [
+        {
+          title: 'Name',
+          dataIndex: 'name',
+          key: 'name',
+          className: 'show',
+        }, {
+          title: 'Age',
+          dataIndex: 'age',
+          key: 'age',
+          className: 'show',
+        }, {
+          title: 'Address',
+          dataIndex: 'address',
+          key: 'address',
+          className: 'show',
+          render: (text, record, index) => (
+            <span>
+            { this.state.selected === index ? <span>Button</span> : '' }
+            </span>
+          ),
+          
+          onFilterDropdownVisibleChange: (visible) => {
+            console.log('wefewf');
+            
+            console.log('wefewf', this.state);
+          },
+        }
+       ],
     };
     this.onCheckBoxChange = this.onCheckBoxChange.bind(this);
     this.onClickSubmit = this.onClickSubmit.bind(this);
+    this.onSortEnd = this.onSortEnd.bind(this);
 
     columns = [
       {
@@ -54,8 +133,12 @@ class App extends Component {
         filterDropdown: (
           <div>
             <div className="custom-filter-dropdown">
-              <Checkbox onChange={this.onCheckBoxChange} name='name'>Name</Checkbox>
-              <Checkbox onChange={this.onCheckBoxChange} name='age'>Age</Checkbox>
+              <SortableContainer onSortEnd={this.onSortEnd}>
+                { this.state.items && this.state.items.map((status, index) => (
+                  <SortableItem key={index} index={index} value={ <Checkbox key={index} checked={status.checked} onChange={this.onCheckBoxChange} name={status.name}>{status.label}</Checkbox>} />
+                  
+                ))}
+              </SortableContainer>
             </div>
             <div>
               <Button type="primary" onClick={this.onClickSubmit}>Done</Button>
@@ -79,31 +162,45 @@ class App extends Component {
     this.setState({ selected: null });
   }
 
-   onCheckBoxChange(e) {
-    if(e.target.checked) {
-      arr.push(e.target.name);
-      this.setState({ filteredColumns: arr });
-    } else {
-      arr.pop(e.target.name);
-      this.setState({ filteredColumns: arr });
-    }
+  onCheckBoxChange(e) {
+    const { items } = this.state;
+    const sample = items && items.map((item) => {
+       if (item.name === e.target.name) {
+         item.checked = e.target.checked;
+      }
+    });
+    this.setState(this.state)
+    return items;
   }
 
   onClickSubmit() {
-    const { filteredColumns } = this.state;
+    const { items } = this.state;
     const col = columns && columns.map((item) => {
       item.className = 'show';
-      filteredColumns.map((item1) => {
-        if (item.dataIndex === item1) {
-          item.className = 'hide';
+      items.map((item1) => {
+        if (item.dataIndex === item1.name) {
+          if(!item1.checked) {
+            item.className = 'hide';
+          }
         }
       });
     });
     this.setState(this.state)
     return columns;
    }
+   
+  onSortEnd(index) {
+      const {items, columns} = this.state;
+      this.setState({items: arrayMove(items, index.oldIndex, index.newIndex)});
+      this.setState({columns: arrayMove(columns, index.oldIndex, index.newIndex)})
+
+  }
 
   render() {
+    const {items, columns} = this.state;
+    console.log({columns});
+    console.log({items});
+
     return (
       <div className="App">
         <Table 
@@ -117,6 +214,14 @@ class App extends Component {
             };
           }}
         /> 
+        <SortableContainer onSortEnd={this.onSortEnd}>
+          { items && items.map((status, index) => (
+             <SortableItem key={status.id} index={index} value={ <Checkbox key={status.id} checked={status.checked} onChange={this.onCheckBoxChange} name={status.name}>{status.label}</Checkbox>} />
+          ))}
+        </SortableContainer>
+        <div>
+          <Button type="primary" onClick={this.onClickSubmit}>Done</Button>
+        </div>
       </div>
     );
   }
